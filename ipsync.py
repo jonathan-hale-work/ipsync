b03a9ca9d490fc8cec2e538f21dce8977d436a80
Jonathan Hale <jonathan.hale.work@gmail.com>
2024-12-26 20:36:12 -0700

    retrieve public ip

--- ipsync.py
+++ ipsync.py
@@ -1,5 +1,6 @@
 import json
 
+import socket
 ip_dict = {
         "public_ip_address" : "192.168.0.1"
         }
@@ -8,4 +9,8 @@ ip_json = json.dumps(ip_dict, indent=4)
 with open("ipsync.json", "w") as outfile:
     outfile.write(ip_json)
 
-    
+import urllib.request
+
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
+print(external_ip)
